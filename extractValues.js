c2a2cfb8f15b63c86fcdf1120ea926756277b013
Camara <victordsgnr@gmail.com>
2020-02-02 00:37:51 -0300

    utils extract

---
+++ extractValues.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+fs.readFile(process.argv[2], (err, data) => {
+  if(err) console.error(err)
+  let argv = JSON.parse(data)
+ 
+  let result = []
+  if(Array.isArray(argv)) {
+    console.info('Processando lista...')
+    for(let stmt of argv) {
+      result.push(stmt[process.argv[3]])
+    }
+    fs.writeFile(`${process.argv[2]}_extract.json`, JSON.stringify(result), () => {})
+  } else {
+    console.info('Processando objeto...')
+    result.push(argv[process.argv[3]])
+    fs.writeFile(`${process.argv[2]}_extract.json`, JSON.stringify(result), () => {})
+  }
+  console.info(`Arquivo ${process.argv[2]}_extract.json gerado`)
+})
